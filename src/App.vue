a27a81b50affd299d75a5357e0f1234c66d29843
vivi.peng <vivi.peng@aplus-digital.com>
2021-12-28 18:48:49 +0800

    1228

---
+++ src/App.vue
@@ -0,0 +1,22 @@
+<template>
+    <div id="app">
+        <!-- <img src="./assets/logo.png" /> -->
+        <router-view></router-view>
+    </div>
+</template>
+
+<script>
+    export default {
+        name: "App",
+        created() {
+            const api = `${process.env.APIPATH}/api/${process.env.CUSTOMPATH}/products`;
+            this.$http.get(api).then((response) => {
+                // console.log(response.data)
+            })
+        },
+    };
+</script>
+
+<style lang="scss">
+@import "./assets/css/main";
+</style>
